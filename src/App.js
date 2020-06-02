f21a54dc07309ad53e002fc7a177448b523e87a9
Manav Mendiratta <mmendiratta@wisc.edu>
2020-06-02 13:50:41 -0500

    added template css and js scroll

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header.js'
+import About from './components/About.js'
+import Resume from './components/Resume';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <About />
+      <Resume />
     </div>
   );
 }
