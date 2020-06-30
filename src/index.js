31913f81f80364f03e164329a3071381b8dc2000
Manav Mendiratta <mmendiratta@wisc.edu>
2020-06-30 17:16:10 -0500

    new local working commit

--- src/index.js
+++ src/index.js
@@ -3,10 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import Amplify from "aws-amplify";
-import awsExports from "./aws-exports";
+// import Amplify from "aws-amplify";
+// import awsExports from "./aws-exports";
 
-Amplify.configure(awsExports);
+// Amplify.configure(awsExports);
 
 ReactDOM.render(
   <React.StrictMode>
